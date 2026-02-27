7bdbd63c83df2c8c24f87c52c16f7a20ec06b084
Adityasharma0709 <7rockyrocky@gmail.com>
2026-02-27 18:52:41 +0530

    fix(cors): add vercel frontend url to allowed origins

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200', // Angular
+    origin: ['http://localhost:4200', 'https://jeevan-setu-frontend.vercel.app'], // Angular & Vercel
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
