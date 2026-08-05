7e2243d883debfecfe4e50eaebdc98bcad5c795d
Adityasharma0709 <7rockyrocky@gmail.com>
2026-08-05 13:56:30 +0530

    feat: initialize application entry point with CORS configuration and global validation pipes while updating package dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true, // allowed origins (reflects request origin)
+    origin: 'https://www.jeevansetu.org',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
@@ -22,32 +21,10 @@ async function bootstrap() {
     }),
   );
 
-  // Swagger setup MUST be before listen()
-  const config = new DocumentBuilder()
-    .setTitle('Jeevan Setu API')
-    .setDescription('Backend APIs')
-    .setVersion('1.0')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'JWT',
-        description: 'Enter JWT token',
-        in: 'header',
-      },
-      'JWT-auth',
-    )
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
   // Listen should ALWAYS be last
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   Logger.log(`Application is running on: ${port}`, 'Bootstrap');
-  Logger.log(`Swagger UI is available at: ${port}/api`, 'Bootstrap');
 }
 
 bootstrap();
