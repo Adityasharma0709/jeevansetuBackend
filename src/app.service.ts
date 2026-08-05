f340ebf6fec8b7c38325830e1b007ce1163f32b9
Adityasharma0709 <7rockyrocky@gmail.com>
2026-08-05 14:27:07 +0530

    feat: initialize backend application with service, controller testing, and global validation pipes

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'backend is running!';
+    return 'server is running';
   }
 }
